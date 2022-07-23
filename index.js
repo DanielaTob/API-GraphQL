5280061b1225d9cc086191b508cae21ed378b71f
daniela <moreno.tobar.daniela@gmail.com>
2022-07-23 17:44:57 -0500

    optimization of code

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 import { createServer } from '@graphql-yoga/node'
 
+let clients = [
+  {
+    'name': 'Peter',
+    'lastName': 'Parker',
+  },
+  {
+    'name': 'Paul',
+    'lastName': 'Perez',
+  },
+  {
+    'name': 'Mary',
+    'lastName': 'kay',
+  },
+  {
+    'name': 'John',
+    'lastName': 'puerto',
+  }
+];
+
+const typeDefinitions = ` 
+    type Query {
+      clients: [Client]
+    },
+    type Client {
+      name: String,
+      lastName: String
+    }
+`;
+
+const resolvers = {
+  Query: {
+    clients: () => {
+      return clients;
+    }
+  }
+}
+
 const server = createServer({
   schema: {
-    typeDefs: /* GraphQL */ `
-      type Query {
-        serverStatus: String
-      }
-    `,
-    resolvers: {
-      Query: {
-        serverStatus: () => 'Working ok',
-      },
-    },
+    typeDefs: typeDefinitions,
+    resolvers: resolvers,
   },
 })
+
 
 server.start()
