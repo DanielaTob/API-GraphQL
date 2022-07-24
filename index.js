05ed21ca52d251b547838543d08d8fae6764d25f
daniela <moreno.tobar.daniela@gmail.com>
2022-07-23 21:07:26 -0500

    schema api nasa

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { createServer } from '@graphql-yoga/node'
+import { createServer } from '@graphql-yoga/node';
+import fetch from 'node-fetch';
 
 let clients = [
   {
@@ -19,13 +20,97 @@ let clients = [
   }
 ];
 
-const typeDefinitions = ` 
+const typeDefinitions = `
     type Query {
-      clients: [Client]
+      clients: [Client],
+      asteroidsNear: AsteroidsNear
     },
+
+    type Mutation {
+      addClients(name: String, lastName: String): Client
+    },
+
     type Client {
       name: String,
       lastName: String
+    },
+
+    type AsteroidsNear {
+      element_count: Int,
+      near_earth_objects: NearEarthObjects,
+      links: Links
+    },
+
+    type Links {
+      next: String,
+      prev: String, 
+      self: String
+    },
+
+    type MissDistance {
+      astronomical: String
+      lunar: String
+      kilometers: String
+      miles: String
+    },
+    
+    type RelativeVelocity {
+      kilometers_per_second: String
+      kilometers_per_hour: String
+      miles_per_hour: String
+    },
+    
+    type CloseApproachData {
+      close_approach_date: String
+      close_approach_date_full: String
+      epoch_date_close_approach: Int
+      orbiting_body: String
+      miss_distance: MissDistance
+      relative_velocity: RelativeVelocity
+    },
+    
+    type Feet {
+      estimated_diameter_min: Float
+      estimated_diameter_max: Float
+    },
+    
+    type Miles {
+      estimated_diameter_min: Float
+      estimated_diameter_max: Float
+    },
+    
+    type Meters {
+      estimated_diameter_min: Float
+      estimated_diameter_max: Float
+    },
+    
+    type Kilometers {
+      estimated_diameter_min: Float
+      estimated_diameter_max: Float
+    },
+    
+    type EstimatedDiameter {
+      feet: Feet
+      miles: Miles
+      meters: Meters
+      kilometers: Kilometers
+    },
+    
+    type Today {
+      id: String
+      neo_reference_id: String
+      name: String
+      nasa_jpl_url: String
+      absolute_magnitude_h: Float
+      is_potentially_hazardous_asteroid: Boolean
+      is_sentry_object: Boolean
+      close_approach_data: [CloseApproachData]
+      estimated_diameter: EstimatedDiameter
+      links: Links
+    },
+    
+    type NearEarthObjects {
+      today: [Today]
     }
 `;
 
@@ -33,6 +118,29 @@ const resolvers = {
   Query: {
     clients: () => {
       return clients;
+    },
+    asteroidsNear: async () => {
+      let res = await fetch('https://www.neowsapp.com/rest/v1/feed?start_date=2022-07-23&end_date=2022-07-23')
+      
+      res = await res.text();
+      
+      console.log(res);
+
+      res = res.replaceAll("2022-07-23", "today");
+
+      res = JSON.parse(res);
+      
+      return res;
+    }
+  },
+  Mutation: {
+    addClients: (_, data ) => {
+      let newClient = {
+        'name': data.name,
+        'lastName': data.lastName
+      };
+      clients.push(newClient);
+      return newClient;
     }
   }
 }
